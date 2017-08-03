8d6305ff40ca0c91f9601eb6a599b4c501677fb4
Dan <dl.carter00@gmail.com>
2017-08-03 08:03:53 -1000

    setup pug, stylus, yarn

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,29 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+  div.hello
+    h1 {{msg}}
+    h2 Essential Links
+    ul
+      li
+        a(href="https://vuejs.org", target="_blank") Core Docs
+      li
+        a(href="https://vuejs.org", target="_blank") Forum
+      li
+        a(href="https://forum.vuejs.org", target="_blank") Gitter Chat
+      li
+        a(href="https://twitter.com/vuejs", target="_blank") Twitter
+      br
+      li
+        a(href="http://vuejs-templates.github.io/webpack/", target="_blank") Docs For This Template
+    h2 Ecosystem
+    ul
+      li
+        a(href="http://router.vuejs.org/", target="_blank") vue-router
+      li
+        a(href="http://vuex.vuejs.org/", target="_blank") vuex
+      li
+        a(href="http://vue-loader.vuejs.org/", target="_blank") vue-loader
+      li
+        a(href="https://github.com/vuejs/awesome-vue", target="_blank") awesome-vue
 </template>
 
 <script>
@@ -25,29 +31,25 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to your working vue app with pug'
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="stylus" scoped>
+h1, h2
+  font-weight: normal
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul
+  list-style-type: none
+  padding: 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li
+  display: inline-block
+  margin: 0 10px
 
-a {
-  color: #42b983;
-}
+a
+  color: #42b983
 </style>
